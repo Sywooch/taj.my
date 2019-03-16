f3f69ae7dd21972bc82995c29866a3e0f2b44d41
nootebook_74 <cheltend201@yandex.ru>
2019-03-16 23:30:40 +0500

    установлен код рекламы adsence

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -25,20 +25,31 @@ AppAsset::register($this);
     <?php $this->head() ?>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    
+
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
-      (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-7101449661048825",
-        enable_page_level_ads: true
-      });
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-7101449661048825",
+            enable_page_level_ads: true
+        });
     </script>
-	<script>
-	  (adsbygoogle = window.adsbygoogle || []).push({
-		google_ad_client: "ca-pub-1910996311142149",
-		enable_page_level_ads: true
-	  });
-	</script>
+
+<!--    устаревшая версия адсенс-->
+<!--    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>-->
+<!--    <script>-->
+<!--      (adsbygoogle = window.adsbygoogle || []).push({-->
+<!--        google_ad_client: "ca-pub-7101449661048825",-->
+<!--        enable_page_level_ads: true-->
+<!--      });-->
+<!--    </script>-->
+<!--	<script>-->
+<!--	  (adsbygoogle = window.adsbygoogle || []).push({-->
+<!--		google_ad_client: "ca-pub-1910996311142149",-->
+<!--		enable_page_level_ads: true-->
+<!--	  });-->
+<!--	</script>-->
+
+
 <!--    Гугл тэг-->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136313284-1"></script>
     <script>
@@ -48,7 +59,7 @@ AppAsset::register($this);
 
         gtag('config', 'UA-136313284-1');
     </script>
-    
+
     <!--    метрика-->
     <!-- Yandex.Metrika counter -->
     <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(52815142, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/52815142" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
