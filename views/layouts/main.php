a222b686529dc2fe2c3f6528229253376ab0ec55
nootebook_74 <cheltend201@yandex.ru>
2019-03-16 23:29:22 +0500

    Установлен код метрики

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -48,6 +48,11 @@ AppAsset::register($this);
 
         gtag('config', 'UA-136313284-1');
     </script>
+    
+    <!--    метрика-->
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(52815142, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/52815142" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 </head>
 <?php $this->beginBody() ?>
 <?= $content ?>
